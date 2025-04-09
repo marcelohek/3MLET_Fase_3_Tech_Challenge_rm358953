9b83482443066bcfb66790986cd9c88e26748a60
marcelohek <89038327+marcelohek@users.noreply.github.com>
2025-04-09 14:59:18 -0300

    Update api.py

--- sisu_app/api.py
+++ sisu_app/api.py
@@ -5,16 +5,7 @@ import joblib
 import pandas as pd
 
 modelo = joblib.load("modelo_xgboost_sisu.joblib")
-
-features = [
-    'QT_VAGAS_CONCORRENCIA',
-    'NOME_CURSO_Engenharia Civil',
-    'SIGLA_IES_UFRJ',
-    'UF_CAMPUS_RJ',
-    'GRAU_Bacharelado',
-    'TURNO_Noturno',
-    'MOD_CONCORRENCIA_AC'
-]
+features = joblib.load("features_xgboost_sisu.pkl")
 
 app = FastAPI()
 
@@ -29,17 +20,19 @@ class EntradaDados(BaseModel):
 
 @app.post("/prever")
 def prever_nota(dados: EntradaDados):
-    entrada_dict = {
-        "QT_VAGAS_CONCORRENCIA": dados.qt_vagas,
-        f"NOME_CURSO_{dados.nome_curso}": 1,
-        f"SIGLA_IES_{dados.sigla_ies}": 1,
-        f"UF_CAMPUS_{dados.uf_campus}": 1,
-        f"GRAU_{dados.grau}": 1,
-        f"TURNO_{dados.turno}": 1,
-        f"MOD_CONCORRENCIA_{dados.mod_concorrencia}": 1,
+    entrada = dados.dict()  
+
+    entrada_dummies = {
+        "QT_VAGAS_CONCORRENCIA": entrada["qt_vagas"],
+        f"NOME_CURSO_{entrada['nome_curso']}": 1,
+        f"SIGLA_IES_{entrada['sigla_ies']}": 1,
+        f"UF_CAMPUS_{entrada['uf_campus']}": 1,
+        f"GRAU_{entrada['grau']}": 1,
+        f"TURNO_{entrada['turno']}": 1,
+        f"MOD_CONCORRENCIA_{entrada['mod_concorrencia']}": 1,
     }
 
-    entrada_final = {col: entrada_dict.get(col, 0) for col in features}
+    entrada_final = {col: entrada_dummies.get(col, 0) for col in features}
     df = pd.DataFrame([entrada_final])
     pred = modelo.predict(df)[0]
-    return {"nota_prevista": round(pred, 2)}
+    return {"nota_prevista": round(float(pred), 2)}  
